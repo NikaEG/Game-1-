e9947d359a325976948703bae76f8f9f83d32fd6
NikaEG <ally.via@gmail.com>
2020-07-20 15:15:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+Materials = {"AAA" : "Copic Markers",
+"AAB" : "Graphite Pencils",
+"ABA" : "Colored Pencils",
+"ABB" : "Crayola Markers",
+"BAA" : "Crayons",
+"BAB" : "Acrylic Paint",
+"BBA" : "Oil Paint",
+"BBB" : "Watercolors",
+"CAA" : "Charcoal",
+"CAB" : "Pastels",
+"CBA" : "Oil Pastels",
+"CBB" : "Digital",
+"DAA" : "Fine Liners",
+"DAB" : "Pens",
+"DBA" : "Gouache Paint",
+"DBB" : "Paint Markers"}
+Choice1 = input("Which color do you prefer? \nA) Blue \nB) Red \n")
+Choice2 = input("Which color palette do you prefer? \nA) Pastels \nB) Vibrants \n")
+Choice3 = input("What tones do you prefer? \nA) Muted Tones \nB) Bright Tones \n")
+Choice4 = input("Are you... \nA) Patient \nB) Inpatient \n")
+Choice5 = input("Are you a... \nA) Hard Worker \nB) Slacker \n")
+Choice6 = input("What type of shading do you prefer? \nA) Harsh Lines \nB) Soft Fades \n")
+Choice7 = input("Which do you prefer? \nA) Anime \nB) Realism \n")
+Choice8 = input("Do you watch anime? \nA) Of course! I'm a weeb and proud of it! \nB) Never. \n")
+Choice9 = input("Do you watch art turorials? \nA) All the time! \nB) Nope. \n")
+Choice10 = input("Do you think digital art is REAL art? \nA) Of course it is! \nB) Digital art isn't real! \n")
